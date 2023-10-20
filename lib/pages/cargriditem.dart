15b4ce6fbb68e04bf950c5215442db89b7f020b7
bhargavp2004 <105142165+bhargavp2004@users.noreply.github.com>
2023-10-20 13:10:50 +0530

    commit

--- lib/pages/cargriditem.dart
+++ lib/pages/cargriditem.dart
@@ -31,80 +31,83 @@ class CarGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListView(
-        shrinkWrap: true,
-        children: [
-          if (imageURL != null && Uri.parse(imageURL).isAbsolute)
-            Image.network(
-              imageURL,
-              fit: BoxFit.fitWidth,
-              height: 120,
-              loadingBuilder: (context, child, loadingProgress) {
-                if (loadingProgress == null) {
-                  return child;
-                } else {
-                  return Center(
-                    child: CircularProgressIndicator(
-                      value: loadingProgress.expectedTotalBytes != null
-                          ? loadingProgress.cumulativeBytesLoaded /
-                              (loadingProgress.expectedTotalBytes ?? 1)
-                          : null,
+     return SizedBox(
+      height: 300,
+      child: Card(
+        child: ListView(
+          shrinkWrap: true,
+          children: [
+            if (imageURL != null && Uri.parse(imageURL).isAbsolute)
+              Image.network(
+                imageURL,
+                fit: BoxFit.fitWidth,
+                height: 120,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress == null) {
+                    return child;
+                  } else {
+                    return Center(
+                      child: CircularProgressIndicator(
+                        value: loadingProgress.expectedTotalBytes != null
+                            ? loadingProgress.cumulativeBytesLoaded /
+                                (loadingProgress.expectedTotalBytes ?? 1)
+                            : null,
+                      ),
+                    );
+                  }
+                },
+                errorBuilder: (context, error, stackTrace) {
+                  return Center(child: Text('Failed to load image'));
+                },
+              ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Container(
+                height: 200,
+                child: Column(
+                  children: [
+                    Text(brand, style: TextStyle(fontWeight: FontWeight.bold)),
+                    Text(model),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => CarDetails(carId: carId)),
+                        );
+                      },
+                      child: Text("View More"),
                     ),
-                  );
-                }
-              },
-              errorBuilder: (context, error, stackTrace) {
-                return Center(child: Text('Failed to load image'));
-              },
-            ),
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Container(
-              height: 200,
-              child: Column(
-                children: [
-                  Text(brand, style: TextStyle(fontWeight: FontWeight.bold)),
-                  Text(model),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => CarDetails(carId: carId)),
-                      );
-                    },
-                    child: Text("View More"),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      // Call the deletePost function when the button is pressed
-                      deletePost(carId);
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.red,
+                    ElevatedButton(
+                      onPressed: () {
+                        // Call the deletePost function when the button is pressed
+                        deletePost(carId);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.red,
+                      ),
+                      child: Text("Delete Post"),
                     ),
-                    child: Text("Delete Post"),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => UpdateCarForm(carId: carId),
-                        ),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green,
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => UpdateCarForm(carId: carId),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                      ),
+                      child: Text("Update Post"),
                     ),
-                    child: Text("Update Post"),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
